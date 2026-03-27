298a391371a406d951b27f48270462ad7789431e
roxy1103200 <roxy1103200@gmail.com>
2026-03-27 21:31:08 +0800

    启用禁用员工账号新增代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,10 +15,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -77,6 +76,11 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
     //员工管理实现员工的增删改查
     @PostMapping
     @ApiOperation("员工新增")
@@ -86,18 +90,28 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
     }
-    @PostMapping("/page")
-    @ApiOperation("员工查询")
-    public Result page(EmployeeDTO employeeDTO){
-        log.info("员工查询:{}" ,employeeDTO);
-        employeeService.page(employeeDTO);
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("员工查询:{}" ,employeePageQueryDTO);
+        PageResult pageResult =  employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+    /**
+     * 编辑员工信息
+     * @param
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("员工状态信息修改")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("员工状态修改：{} {}",status, id);
+        employeeService.startOrStop(status,id);
         return Result.success();
     }
-//    @ApiOperation("员工修改")
-//    @PostMapping("/update")
-//    public Result update(@RequestBody EmployeeDTO employeeDTO){
-//        log.info("员工修改:{}" ,employeeDTO);
-//        employeeService.update(employeeDTO);
-//        return Result.success();
-//    }
 }
